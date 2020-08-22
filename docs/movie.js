c0bbc34bdcacab85403e94539602a80af1b1a530
Ryan Mendez <rjmendez80@gmail.com>
2020-08-22 01:40:14 -0700

    debugging movie by genre

--- docs/movie.js
+++ docs/movie.js
@@ -1,64 +1,38 @@
 $(document).ready(function () {
     Math.random();
-    function getRandomInt(max) {
+    function getRandomindex(max) {
         return Math.floor(Math.random() * Math.floor(max));
       }
-    // Latest ID entry for MDB is 734982 below we generate a random number 
-    // All movies, not very realistic to use this option has you can see it contains 743,000 + titles incluing Adult titles wtf..
-    // looking for ways to get less films.
+    function returnResults(response,index) {
+        console.log(response.results[index].title);
+        console.log(response.results[index].release_date);
+        console.log(response.results[index].overview);
+        console.log(response.results[index].vote_average);
+        console.log(response.results[index].poster_path);
+        console.log(response.results[index].video);
+       
 
-    // generate random ID
-    // let randomId = getRandomInt((734982)+1);
-    // console.log(randomId);  
-    // const allMoviesUrl = 'https://api.themoviedb.org/3/movie/';   
-    // const KeyForRandom = '?api_key=3005c900380601fd47b2b821bbb3a101';
-
-        
-    // $.ajax({
-    //     url: allMoviesUrl + randomId + KeyForRandom,
-    //     method: 'GET',
-    // }).then(function(response){
-    //     console.log(response);
-    //     console.log(response.title);
-        // console.log(response.overview);
-        // console.log(response.id);
-        // console.log(response.genres);
-    
-    // })
-    // define variables to construct the url 
+    }  
+   
+     // define variables to construct the url 
     const discoverUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=';
     const key = '3005c900380601fd47b2b821bbb3a101'
     const language = '&language=en-US';
     const sort = '&sort_by=';
     const sortOptions = ['popularity.desc','release_date.desc', 'primary_release_date.desc','vote_avrage.desc'];
-    let pageNum = getRandomInt((500)+1);
-    let randomIndex = getRandomInt(20);
+    let pageindex = getRandomindex((500)+1);
+    let randomIndex = getRandomindex(20);
     console.log(randomIndex);
-    // ajax request for a movies by current popularity excluding adult titles 
+    // ajax request for a random movie 
     $.ajax({
-        url: discoverUrl + key + language + sort + sortOptions[0] + '&include_adult=false&include_video=false&page='+ pageNum,
+        url: discoverUrl + key + language + sort + sortOptions[0] + '&include_adult=false&include_video=false&page='+ pageindex,
         method: 'GET',
     }).then(function(response){
         console.log(response);
-        // title of 
-        console.log(response.results[randomIndex].title);
-        console.log(response.results[randomIndex].release_date);
-        console.log(response.results[randomIndex].overview);
-        console.log(response.results[randomIndex].vote_average);
-        console.log(response.results[randomIndex].poster_path);
-        console.log(response.results[randomIndex].video);
-       
-    
-    })
-    $.ajax({
-        url: discoverUrl + key + language + sort + sortOptions[3] + '&include_adult=false&include_video=false&page=1',
-        method: 'GET',
-    }).then(function(response){
-        console.log(response);
-       
-    
-    })
-  
+        returnResults(response,randomIndex); 
+     })
+   
+        // array of object that contain MDB genres and coorisponding ID tag.
         let genres = [
           {
             "id": 28,
@@ -137,11 +111,22 @@ $(document).ready(function () {
             "name": "Western"
           }
         ]
+        const length = Object.keys(genres).length;
+        const randomNumber = Math.floor((Math.random() * length) + 0);
+        let randomGenreID = genres[Object.keys(genres)[randomNumber]];
+        console.log(randomGenreID.id)
+
+
+        // let randomGenreID = genres.id[Math.floor(Math.random() * genres.length)];
+        // console.log(randomGenreID);
+        
         $.ajax({
-            url: discoverUrl + key + language + '&with_genres=' + 37,
+            url: discoverUrl + key + language + '&with_genres=' + randomGenreID.id +'&page='+ pageindex,
             method: 'GET',
         }).then(function(response){
             console.log(response);
+            console.log(response.total_results);
+            // returnResults(response,randomIndex);
            
         
         })
