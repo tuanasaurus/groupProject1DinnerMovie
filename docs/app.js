52311e6003369f3810765f9c2eaf90516af1f1ec
Tuan Phan <tuan@tuan.design>
2020-08-25 18:52:26 -0700

    Updated files

--- docs/app.js
+++ docs/app.js
@@ -29,6 +29,7 @@ $(document).ready(function () {
   const apiKey =
     "2w5brNs3HF2ABKHAAT-I-QePQxcgY5vkGcva4WnpZxo1Dn9CLUevWblrCofVxQJymD3JEgh9JXfDDv3qRbTtBO-AV7TskU-h0fvMXwG1pdXc12iCTgiiLnThOvw9X3Yx";
   //click functionality to the Locate button to get a random restaurant based on location
+
   $("#locateBtn").click(function () {
     let location = $("#myLocation").val().trim();
     location = encodeURI(location);
@@ -50,9 +51,9 @@ $(document).ready(function () {
       let restaurants = response.businesses[randomRestaurant].name;
       console.log("randomRestaurant", randomRestaurant);
       console.log("restaurants", restaurants);
-
+      
       //to target the selected option
-      let cuisinePick = $("#cuisineSelect").val();
+      let cuisinePick = $(".cuisineSelect").val();
       console.log(cuisinePick);
 
       //This function loops through the list of restaurants to find 3 random ones
@@ -70,7 +71,7 @@ $(document).ready(function () {
           //and then loops through the 3 randomly selected restaurants' categories array
           for (let index = 0; index < categories.length; index++) {
             //and then check if the categoriesArray set on line 21 matches the object categories
-            if (categoriesArray.includes(categories[index].alias)) {
+            if (cuisinePick === categoriesArray.includes(categories[index].alias)) {
               console.log(restaurant);
               // this is for appending the restaurant name and other values
               $("#name").append.text(restaurant);
@@ -82,6 +83,7 @@ $(document).ready(function () {
               break;
             }
           }
+      
           console.log(randomRestaurant);
         }
       });
@@ -161,6 +163,10 @@ $(document).ready(function () {
     // let previewOne = 'https://www.youtube.com/watch?v=' + previewKey
     // console.log(previewOne)
   });
+
+
+
+  
   // ajax request for random film, to use with genrate random movie.
   $.ajax({
     // adding options in the url to sort by populariy, exclude adult titles and select a random page number.
@@ -174,7 +180,7 @@ $(document).ready(function () {
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    let randomFilm = response.results[randomIndex];
+    let randomFilm = response.results[0];
     // console.log(randomFilm)
     let randomFilmID = randomFilm.id;
     // console.log(randomFilmID);
@@ -185,17 +191,20 @@ $(document).ready(function () {
     let randomFilmPoster = randomFilm.poster_path;
     console.log(randomFilmPoster);
 
-
+    $("#movieBtnRandom").click(function(){
     const movieTitle = $("<h1 class='card-title'>").text(randomFilmTitle);
     const movieInfo = $("<p class='card-author'>").text(randomFilmInfo);
     const moviePoster = $("<img class='card-image'>").attr("scr", randomFilmPoster);
     $("#movieCard1").append(moviePoster, movieTitle, movieInfo );
-
+    });
     // calling a call back function
     getMoviePreview(randomFilmID, function (previewUrl) {
       console.log(previewUrl);
     });
   });
+
+
+  
 
   $.ajax({
     // adding options in the url to sort by populariy, exclude adult titles and select a random page number.
@@ -220,12 +229,12 @@ $(document).ready(function () {
     let randomFilmPoster = randomFilm.poster_path;
     console.log(randomFilmPoster);
 
-
+    $("#movieBtnRandom").click(function(){
     const movieTitle = $("<h1 class='card-title'>").text(randomFilmTitle);
     const movieInfo = $("<p class='card-author'>").text(randomFilmInfo);
     const moviePoster = $("<img class='card-image'>").attr("scr", randomFilmPoster);
     $("#movieCard2").append(moviePoster, movieTitle, movieInfo );
-
+    });
     // calling a call back function
     getMoviePreview(randomFilmID, function (previewUrl) {
       console.log(previewUrl);
@@ -244,7 +253,7 @@ $(document).ready(function () {
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    let randomFilm = response.results[randomIndex];
+    let randomFilm = response.results[5];
     // console.log(randomFilm)
     let randomFilmID = randomFilm.id;
     // console.log(randomFilmID);
@@ -255,12 +264,12 @@ $(document).ready(function () {
     let randomFilmPoster = randomFilm.poster_path;
     console.log(randomFilmPoster);
 
-
+    $("#movieBtnRandom").click(function(){
     const movieTitle = $("<h1 class='card-title'>").text(randomFilmTitle);
     const movieInfo = $("<p class='card-author'>").text(randomFilmInfo);
     const moviePoster = $("<img class='card-image'>").attr("scr", randomFilmPoster);
     $("#movieCard3").append(moviePoster, movieTitle, movieInfo );
-
+    });
     // calling a call back function
     getMoviePreview(randomFilmID, function (previewUrl) {
       console.log(previewUrl);
@@ -272,79 +281,79 @@ $(document).ready(function () {
   let genres = [
     {
       id: 28,
-      name: "Action",
+      name: "action",
     },
     {
       id: 12,
-      name: "Adventure",
+      name: "adventure",
     },
     {
       id: 16,
-      name: "Animation",
+      name: "animation",
     },
     {
       id: 35,
-      name: "Comedy",
+      name: "comedy",
     },
     {
       id: 80,
-      name: "Crime",
+      name: "crime",
     },
     {
       id: 99,
-      name: "Documentary",
+      name: "documentary",
     },
     {
       id: 18,
-      name: "Drama",
+      name: "drama",
     },
     {
       id: 10751,
-      name: "Family",
+      name: "family",
     },
     {
       id: 14,
-      name: "Fantasy",
+      name: "fantasy",
     },
     {
       id: 36,
-      name: "History",
+      name: "history",
     },
     {
       id: 27,
-      name: "Horror",
+      name: "horror",
     },
     {
       id: 10402,
-      name: "Music",
+      name: "music",
     },
     {
       id: 9648,
-      name: "Mystery",
+      name: "mystery",
     },
     {
       id: 10749,
-      name: "Romance",
+      name: "romance",
     },
     {
       id: 878,
-      name: "Science Fiction",
+      name: "scienceFiction",
     },
     {
       id: 10770,
-      name: "TV Movie",
+      name: "tvMovie",
     },
     {
       id: 53,
-      name: "Thriller",
+      name: "thriller",
     },
     {
       id: 10752,
-      name: "War",
+      name: "war",
     },
     {
       id: 37,
-      name: "Western",
+      name: "western",
     },
   ];
   const length = Object.keys(genres).length;
@@ -369,14 +378,55 @@ $(document).ready(function () {
     // let randomPage = getRandomNum(response.total_pages);
     // console.log(randomPage)
     let randomGenreFilm = response.results[randomIndex];
-    // console.log(randomGenreFilm);
     let randomGenreTitle = randomGenreFilm.title;
-    // console.log(randomGenreTitle);
     let randomGenreInfo = randomGenreFilm.overview;
-    // console.log(randomGenreInfo);
     let randomGenrePoster = randomGenreFilm.poster_path;
-    // console.log(randomGenrePoster);
-  });
+
+    let randomGenreFilm2 = response.results[5];
+    let randomGenreTitle2 = randomGenreFilm2.title;
+    let randomGenreInfo2 = randomGenreFilm2.overview;
+    let randomGenrePoster2 = randomGenreFilm2.poster_path;
+
+    let randomGenreFilm3 = response.results[10];
+    let randomGenreTitle3 = randomGenreFilm3.title;
+    let randomGenreInfo3 = randomGenreFilm3.overview;
+    let randomGenrePoster3 = randomGenreFilm3.poster_path;
+
+    const genreSelection = $("#genreSelect").val();  
+
+    // $("#movieBtnGenre").click(function(){
+    //   for(var i=1; i<genres.length; i++){
+    //     if(genreSelection === genres[i].name) {
+    //       const movieTitle = $("<h1 class='card-title'>").text(randomGenreTitle);
+    //       const movieInfo = $("<p class='card-author'>").text(randomGenreInfo);
+    //       const moviePoster = $("<img class='card-image'>").attr("scr", randomGenrePoster);
+    //   $("#movieCard1").append(moviePoster, movieTitle, movieInfo )
+    //   console.log("testgenre", genreSelection);
+    //     }
+    //   }
+    // });
+
+
+    });
+
+    // $("#movieBtnGenre").click(function(){
+    //   if(value === genres.name){
+    //     const movieTitle = $("<h1 class='card-title'>").text(randomGenreTitle2);
+    //     const movieInfo = $("<p class='card-author'>").text(randomGenreInfo2);
+    //     const moviePoster = $("<img class='card-image'>").attr("scr", randomGenrePoster2);
+    //     $("#movieCard2").append(moviePoster, movieTitle, movieInfo )
+    //   }
+    // });
+
+    // $("#movieBtnGenre").click(function(){
+    //   if(value === genres.name){
+    //     const movieTitle = $("<h1 class='card-title'>").text(randomGenreTitle3);
+    //     const movieInfo = $("<p class='card-author'>").text(randomGenreInfo3);
+    //     const moviePoster = $("<img class='card-image'>").attr("scr", randomGenrePoster3);
+    //     $("#movieCard3").append(moviePoster, movieTitle, movieInfo )
+    //   }
+    // });
+
 
 
 
